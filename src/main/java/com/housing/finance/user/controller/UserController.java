5312afa01318b4fe8ed01382c8696420252d57c2
korea8378 <korea8378@naver.com>
2019-10-06 01:55:51 +0900

    [#7][ADD] 유저 회원가입 API

---
+++ src/main/java/com/housing/finance/user/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.housing.finance.user.controller;
+
+import com.housing.finance.user.dto.ReqSignUpDto;
+import com.housing.finance.user.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> signUp(@RequestBody @Valid ReqSignUpDto reqSignUpDto, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            throw new NullPointerException();
+        }
+
+        userService.signUp(reqSignUpDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
---
+++ src/main/java/com/housing/finance/user/dto/ReqSignUpDto.java
@@ -0,0 +1,13 @@
+package com.housing.finance.user.dto;
+
+        import lombok.Getter;
+        import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ReqSignUpDto {
+
+    private String userId;
+
+    private String password;
+}
