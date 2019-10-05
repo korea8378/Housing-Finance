465735f4a35bf6fe1d18e62bbc30d8fcd41eca0e
korea8378 <korea8378@naver.com>
2019-10-06 00:59:41 +0900

    [#8][ADD] 유저 엔티티 맵핑

---
+++ src/main/java/com/housing/finance/user/domain/User.java
@@ -0,0 +1,22 @@
+package com.housing.finance.user.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String userId;
+
+    private String password;
+}
