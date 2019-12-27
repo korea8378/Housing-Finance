8990765a1503c8609a47216171f96c39443b8e15
korea8378 <korea8378@naver.com>
2019-12-27 17:52:19 +0900

    rebase merage test용

--- src/main/java/com/housing/finance/FinanceApplication.java
+++ src/main/java/com/housing/finance/FinanceApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FinanceApplication {
 
+    //rebase test
     public static void main(String[] args) {
         SpringApplication.run(FinanceApplication.class, args);
     }
 
 }
+용
